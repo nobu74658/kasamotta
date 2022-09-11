edd7187fada49c136491f5ea421e488ad0d33c1e
nobu <hiroki-pon@outlook.jp>
2022-09-11 23:45:25 +0900

    test

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const FCMToken = admin.database().ref(`/FCMTokens/${userId}`).once('value');
+
+const payload = {
+	token: FCMToken,
+    notification: {
+        title: 'cloud function demo',
+        body: message
+    },
+    data: {
+        body: message,
+    }
+};
+
+admin.messaging().send(payload).then((response) => {
+    // Response is a message ID string.
+    console.log('Successfully sent message:', response);
+    return {success: true};
+}).catch((error) => {
+    return {error: error.code};
+});
